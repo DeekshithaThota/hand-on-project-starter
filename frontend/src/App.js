d698cc618d31b2d03492d0aeba83bfd3489a359d
DeekshithaThota <dhopathiramani@gmail.com>
2022-07-27 23:29:43 +0530

    1st git commit :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
+import React from 'react'
+//import Dashboard from "./pages/dashboard/Dashboard"
 import logo from './logo.svg';
 import './App.css';
+//import Upload from './pages/upload/Upload'
+import Loggedin from './pages/loggedin/Loggedin'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import DashLoggedin from './pages/DashLoggedin/DashLoggedin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    <Router>
+      <nav>
+        <img src={logo} className='logo' />
+      </nav>
+     
+      <Routes>
+        {/* <Route path="/" element={<Dashboard/>} /> */}
+        <Route path="/" element={<DashLoggedin/>} />
+        <Route path="pages/upload/Upload" element={<Loggedin/>} />
+
+      </Routes>
+    </Router>
+
+  )
 }
 
-export default App;
+export default App
